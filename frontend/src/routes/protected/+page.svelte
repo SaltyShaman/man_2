52448676e1ddaefb03c06ba181ba079e7a505a03
SaltyShaman <kristoffer6400@gmail.com>
2025-11-21 17:46:06 +0100

    Protected page works

---
+++ frontend/src/routes/protected/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+
+  let user = {};
+  let message = '';
+
+  async function fetchProtected() {
+    try {
+      const res = await fetch('http://localhost:8080/protected', {
+        credentials: 'include'
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        user = data.user || {};
+      } else {
+        message = 'You are not logged in. Redirecting to login...';
+        setTimeout(() => goto('/login'), 1500);
+      }
+    } catch (err) {
+      message = 'Error fetching protected data';
+      console.error(err);
+    }
+  }
+
+  async function logout() {
+    await fetch('http://localhost:8080/auth/logout', {
+      method: 'POST',
+      credentials: 'include'
+    });
+    goto('/login');
+  }
+
+  onMount(() => fetchProtected());
+</script>
+
+<h1>Protected Page</h1>
+
+{#if Object.keys(user).length > 0}
+  <p>Welcome, {user.username}! Role: {user.role}</p>
+  <button on:click={logout}>Logout</button>
+{:else}
+  <p>{message}</p>
+{/if}
